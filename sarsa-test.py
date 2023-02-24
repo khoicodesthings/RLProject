200fa133a3f666946c461f69f7ba2ff1b7a5e519
khoicodesthings <khoitrinh@ou.edu>
2023-02-24 12:58:04 -0600

    Organizing

--- sarsa-test.py
+++ sarsa-test.py
@@ -13,16 +13,16 @@ def bins(clip_min, clip_max, num):
 # Discretizing the state
 # Because CartPole has continuous state
 # We need to discretize it
-def digitize_state(observation):
+def discretized(observation):
     # print(observation)
     cart_pos, cart_v, pole_angle, pole_v = observation
     digitized = [
-        np.digitize(cart_pos, bins=bins(-2.4, 2.4, num_dizitized)),
-        np.digitize(cart_v, bins=bins(-3.0, 3.0, num_dizitized)),
-        np.digitize(pole_angle, bins=bins(-0.5, 0.5, num_dizitized)),
-        np.digitize(pole_v, bins=bins(-2.0, 2.0, num_dizitized))
+        np.digitize(cart_pos, bins=bins(-2.4, 2.4, num_discretized)),
+        np.digitize(cart_v, bins=bins(-3.0, 3.0, num_discretized)),
+        np.digitize(pole_angle, bins=bins(-0.5, 0.5, num_discretized)),
+        np.digitize(pole_v, bins=bins(-2.0, 2.0, num_discretized))
     ]
-    return sum([x * (num_dizitized**i) for i, x in enumerate(digitized)])
+    return sum([x * (num_discretized**i) for i, x in enumerate(digitized)])
 
 # Epsilon-greedy method
 def get_action(next_state, episode):  # Gradually take only optimal actions, epsilon-greedy method
@@ -35,10 +35,11 @@ def get_action(next_state, episode):  # Gradually take only optimal actions, eps
 
 # Q-table
 def update_q(q_table, state, action, reward, next_state, next_action):
-    gamma = 0.99
+    gamma = 0.9
     alpha = 0.5
-    q_table[state, action] = (1 - alpha) * q_table[state, action] +\
-            alpha * (reward + gamma * q_table[next_state, next_action])
+    # Our friend Bellman
+    # Q function
+    q_table[state, action] = (1 - alpha) * q_table[state, action] + alpha * (reward + gamma * q_table[next_state, next_action])
 
     return q_table
 
@@ -46,11 +47,11 @@ def update_q(q_table, state, action, reward, next_state, next_action):
 env = gym.make('CartPole-v1')
 max_number_of_steps = 500  # maximum length of each episode
 num_consecutive_iterations = 100  # Number of trials used to evaluate learning completion
-num_episodes = 4000  # Total number of trials
+num_episodes = 1000  # Total number of trials
 goal_average_reward = 475  # maximum rewards value
-num_dizitized = 6  # Number of divisions of the state
+num_discretized = 6  # Number of divisions of the state
 q_table = np.random.uniform(low=-1, high=1, 
-    size=(num_dizitized**4, env.action_space.n))
+    size=(num_discretized**4, env.action_space.n))
 total_reward_vec = np.zeros(num_consecutive_iterations)  # Store the reward of each trial
 final_x = np.zeros((num_episodes, 1))  # Store the x position of the cart at t=200 after learning
 islearned = 0  # Flag to check if learning is done
@@ -65,10 +66,10 @@ for episode in range(1, num_episodes+1):  # repeat for the number of trials
     episodelist.append(episode)
     # Initialize the environment
     observation = env.reset()
-    state = digitize_state(observation[0])
+    state = discretized(observation[0])
     action = np.argmax(q_table[state])
     episode_reward = 0
-
+    #print(episodelist)
     for t in range(max_number_of_steps):  # loop for one trial
         if islearned == 1:  # if learning is finished, render cartPole
             env.render()
@@ -77,43 +78,44 @@ for episode in range(1, num_episodes+1):  # repeat for the number of trials
             env.close()
             break
 
-        # Calculate s_{t+1}, r_{t}, etc. by executing action a_t
+        # get the next state, reward, etc.
+        # for some reason, not having 'extra' breaks the code
         observation, reward, done, info, extra = env.step(action)
 
         # Set reward and penalty
         if done:
             if t < 475:
-                reward = -10  # penalty if it falls down
+                reward = -10  # penalty if the episode fails
             else:
                 reward = 1  # no penalty if it remains upright
         else:
             reward = 1  # reward for standing at each step
 
         episode_reward += reward  # add reward
-
         # Calculate discrete state s_{t+1}
-        next_state = digitize_state(observation)  # convert the observation state at t+1 to a discrete value
+        next_state = discretized(observation)  # convert the observation state at t+1 to a discrete value
 
         # *This is different from Q-learning*
-        next_action = get_action(next_state, episode)  # calculate the next action a_{t+1}
-        q_table = update_q(q_table, state, action, reward, next_state, next_action)
+        next_action = get_action(next_state, episode) # get the next action
+        q_table = update_q(q_table, state, action, reward, next_state, next_action) # update the q values
 
         # Update the next action and state
-        action = next_action  # a_{t+1}
-        state = next_state  # s_{t+1}
+        action = next_action
+        state = next_state
 
         # Processing at the end
         if done:
             print('Episode %d finished after %d time steps / with score %d and mean %f' %
                   (episode, t, episode_reward, total_reward_vec.mean()))
+            
             total_reward_vec = np.hstack((total_reward_vec[1:], episode_reward))  # record reward
-            scorelist.append(total_reward_vec.mean())
+            #print(scorelist)
             if islearned == 1:  # if learning is finished, store the final x-coordinate
                 final_x[episode, 0] = observation[0]
             break
 
-    #if (total_reward_vec.mean() >= goal_average_reward):  # if the recent 100 episodes have achieved the goal reward or more, the training is successful
-    if episode_reward >= goal_average_reward:
+    if (total_reward_vec.mean() >= goal_average_reward):  # if the recent 100 episodes have achieved the goal reward or more, the training is successful
+    #if episode_reward >= goal_average_reward:
         print('Episode %d train agent successfully!' % episode)
         print('After', t, 'time steps')
         print('Average reward is', total_reward_vec.mean())
@@ -127,5 +129,9 @@ for episode in range(1, num_episodes+1):  # repeat for the number of trials
     if islearned == 1:
         break
 
+print(len(episodelist))
+print(len(scorelist))
+#plt.scatter(episodelist, scorelist)
+#plt.show()
 # if islearned:
 #    np.savetxt('final_x.csv', final_x, delimiter=",")  # save the final x-coordinate
